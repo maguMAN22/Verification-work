09e0ce46fd6b47064c95abeb7a9744d3efa0c7bd
Gleb Filippov <glebanov7991@gmail.com>
2023-04-23 19:11:03 +0300

    Add PrintArray

--- Program.cs
+++ Program.cs
@@ -23,4 +23,14 @@
         }
         return arr2;
     }
+    void PrintStringArray(string[] array1)
+    {
+        for (int i = 0; i < array1.Length; i++)
+        {
+            Console.Write(array1[i] + " ");
+        }
+        Console.WriteLine();
+    }
+    PrintStringArray(ReassembleArray(array));
 }
+ReassembleTheArray();
