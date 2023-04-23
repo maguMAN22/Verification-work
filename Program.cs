fa833b8dbea81fa02aca9b908ef455108726f680
Gleb Filippov <glebanov7991@gmail.com>
2023-04-23 19:09:12 +0300

    Add ReassembleArray

--- Program.cs
+++ Program.cs
@@ -5,4 +5,22 @@
     // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
     // При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
     string[] array = { "c#", "Mil", "consider", "IO", "max", "OMEGA", "dd", "reveal", "Gain", "Get" };
+    string[] ReassembleArray(string[] arr1)
+    {
+        int counter = 0;
+        for (int i = 0; i < arr1.Length; i++)
+        {
+            if (arr1[i].Length <= 3)
+            {
+                arr1[counter] = arr1[i];
+                counter++;
+            }
+        }
+        string[] arr2 = new string[counter];
+        for (int a = 0; a < counter; a++)
+        {
+            arr2[a] = arr1[a];
+        }
+        return arr2;
+    }
 }
